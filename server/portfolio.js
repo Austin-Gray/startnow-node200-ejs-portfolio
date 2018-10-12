6ea3b58e4a1b26d40b679e654136ab9f00fec802
Austin <austin.gray4292@gmail.com>
2018-10-12 13:46:30 -0700

    adds pictures of recent projects and respective links to portfolio page

--- server/portfolio.js
+++ server/portfolio.js
@@ -5,20 +5,4 @@ router.get('/', function (req, res) {
   res.render('portfolio')
 });
 
-router.get('/sd-topspots', function (req, res) {
-  res.send('Topspots page')
-});
-
-router.get('/change-calculator', function (req, res) {
-  res.send('Change Calculator page')
-});
-
-router.get('/slango', function (req, res) {
-  res.send('Slango page')
-});
-
-router.get('/vstda', function (req, res) {
-  res.send('vstda page')
-});
-
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -36,6 +36,6 @@ app.post('/thanks', (req, res) => {
     res.render('thanks', { contact: req.body })
 });
 
-app.listen(8080, () => {
-    console.log('listening at http://localhost:8080');
-});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
